a8ccec80ca4a37e473ca8c03459c5f24ae2338ad
tenny314159 <139668743+tenny314159@users.noreply.github.com>
2024-11-19 01:36:45 +0800

    Add files via upload

---
+++ 09softmax regression framework-based.py
@@ -0,0 +1,177 @@
+import torch
+from IPython import display
+from d2l import torch as d2l
+from torchvision import transforms
+import torchvision
+from torch.utils import data
+from torch import nn
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """下载Fashion-MNIST数据集，然后将其加载到内存中"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="data", train=False, transform=trans, download=True)
+    # return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+    #                         num_workers=get_dataloader_workers()),
+    #         data.DataLoader(mnist_test, batch_size, shuffle=False,
+    #                         num_workers=get_dataloader_workers()))
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=0),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=0))
+
+
+def accuracy(y_hat, y):  # @save
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+class Accumulator:
+    """在n个变量上累加"""
+
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """训练模型一个迭代周期（定义见第3章）"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+class Animator:  # @save
+    """在动画中绘制数据"""
+
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+
+def evaluate_accuracy(net, data_iter):  # @save
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # 将模型设置为评估模式
+    metric = Accumulator(2)  # 正确预测数、预测总数
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):  # @save
+    """训练模型（定义见第3章）"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs],
+                        #ylim=[0.3, 0.9],
+                        ylim=[0.05, 1],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+
+net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
+
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+
+net.apply(init_weights)
+loss = nn.CrossEntropyLoss()
+
+# loss = nn.CrossEntropyLoss(reduction='none')改为loss = nn.CrossEntropyLoss()
+# nn.CrossEntropyLoss()
+# 函数是 PyTorch 中用于计算交叉熵损失的函数。
+# 其中 reduction 参数用于控制输出损失的形式。
+# 当 reduction='none' 时，函数会输出一个形状为 (batch_size, num_classes) 的矩阵，表示每个样本的每个类别的损失。
+# 当 reduction='sum' 时，函数会对矩阵求和，输出一个标量，表示所有样本的损失之和。
+# 当 reduction='elementwise_mean' 时，函数会对矩阵求平均，输出一个标量，表示所有样本的平均损失。
+# 在您的例子中，在使用 reduction='none' 时无法训练，是因为需要一个标量来表示整个训练集的损失，而不是一个矩阵。
+# 而使用 reduction='sum' 时，会报错“AssertionError: 761.4056615234375”，可能是因为在某个时刻，损失值变得非常大，导致网络无法继续训练。
+# 只有在使用 reduction='elementwise_mean' 时，将矩阵求平均，使得损失值保持在一个可接受的范围内，网络才能继续训练。
+# 在选择 reduction 时，需要根据具体情况来决定使用哪种方式来计算损失，以保证网络能够正常训练。
+
+trainer = torch.optim.SGD(net.parameters(), lr=0.1)
+num_epochs = 10
+train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
+d2l.plt.show()
