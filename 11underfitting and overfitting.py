8793af6c886e85cbf362b5043c77c64a246fc1af
tenny314159 <139668743+tenny314159@users.noreply.github.com>
2024-11-21 13:58:43 +0800

    Add files via upload

---
+++ 11underfitting and overfitting.py
@@ -0,0 +1,122 @@
+import math
+import numpy as np
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+max_degree = 20  # 多项式的最大阶数
+n_train, n_test = 100, 100  # 训练和测试数据集大小
+true_w = np.zeros(max_degree)  # 分配大量的空间
+true_w[0:4] = np.array([5, 1.2, -3.4, 5.6])
+
+features = np.random.normal(size=(n_train + n_test, 1))
+np.random.shuffle(features)
+poly_features = np.power(features, np.arange(max_degree).reshape(1, -1))
+for i in range(max_degree):
+    poly_features[:, i] /= math.gamma(i + 1)  # gamma(n)=(n-1)!
+# labels的维度:(n_train+n_test,)
+labels = np.dot(poly_features, true_w)
+labels += np.random.normal(scale=0.1, size=labels.shape)
+
+# NumPy ndarray转换为tensor
+true_w, features, poly_features, labels = [torch.tensor(x, dtype=
+torch.float32) for x in [true_w, features, poly_features, labels]]
+
+print(features[:2], poly_features[:2, :], labels[:2])
+# tensor([[-0.3099],
+#         [-0.2983]]) tensor([[ 1.0000e+00, -3.0992e-01,  4.8026e-02, -4.9614e-03,  3.8441e-04,
+#          -2.3827e-05,  1.2308e-06, -5.4492e-08,  2.1110e-09, -7.2694e-11,
+#           2.2530e-12, -6.3476e-14,  1.6394e-15, -3.9083e-17,  8.6519e-19,
+#          -1.7876e-20,  3.4626e-22, -6.3126e-24,  1.0869e-25, -1.7729e-27],
+#         [ 1.0000e+00, -2.9834e-01,  4.4502e-02, -4.4256e-03,  3.3008e-04,
+#          -1.9695e-05,  9.7929e-07, -4.1737e-08,  1.5565e-09, -5.1594e-11,
+#           1.5392e-12, -4.1747e-14,  1.0379e-15, -2.3818e-17,  5.0756e-19,
+#          -1.0095e-20,  1.8823e-22, -3.3033e-24,  5.4750e-26, -8.5969e-28]]) tensor([4.6545, 4.4140])
+
+
+def evaluate_loss(net, data_iter, loss):  # @save
+    """评估给定数据集上模型的损失"""
+    metric = d2l.Accumulator(2)  # 损失的总和,样本数量
+    for X, y in data_iter:
+        out = net(X)
+        y = y.reshape(out.shape)
+        l = loss(out, y)
+        metric.add(l.sum(), l.numel())
+    return metric[0] / metric[1]
+
+
+class Accumulator:
+    """在n个变量上累加"""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def accuracy(y_hat, y):  # @save
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """训练模型一个迭代周期（定义见第3章）"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def train(train_features, test_features, train_labels, test_labels,
+          num_epochs=400):
+    loss = nn.MSELoss()
+    input_shape = train_features.shape[-1]
+    # 不设置偏置，因为我们已经在多项式中实现了它
+    net = nn.Sequential(nn.Linear(input_shape, 1, bias=False))
+    batch_size = min(10, train_labels.shape[0])
+    train_iter = d2l.load_array((train_features, train_labels.reshape(-1, 1)),
+                                batch_size)
+    test_iter = d2l.load_array((test_features, test_labels.reshape(-1, 1)),
+                               batch_size, is_train=False)
+    trainer = torch.optim.SGD(net.parameters(), lr=0.01)
+    animator = d2l.Animator(xlabel='epoch', ylabel='loss', yscale='log',
+                            xlim=[1, num_epochs], ylim=[1e-3, 1e2],
+                            legend=['train', 'test'])
+    for epoch in range(num_epochs):
+        train_epoch_ch3(net, train_iter, loss, trainer)
+        if epoch == 0 or (epoch + 1) % 20 == 0:
+            animator.add(epoch + 1, (evaluate_loss(net, train_iter, loss),
+                                     evaluate_loss(net, test_iter, loss)))
+    print('weight:', net[0].weight.data.numpy())
+    # weight: [[5.0051656  1.221635 - 3.40746    5.577357]]
+
+
+# 从多项式特征中选择前4个维度，即1,x,x^2/2!,x^3/3!
+train(poly_features[:n_train, :4], poly_features[n_train:, :4], labels[:n_train], labels[n_train:])
+
